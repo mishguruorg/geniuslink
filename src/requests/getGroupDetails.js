33810a92f213f63cbb55249b65721fb125e90e44
Lucy-Jane <lucy-jane@mish.guru>
2017-10-17 12:12:33 +1300

    Adds getGroupDetails method and tests

---
+++ src/requests/getGroupDetails.js
@@ -0,0 +1,10 @@
+import { makeGetRequest } from '../request'
+
+const path = '/v1/group/get-details'
+
+const getGroupDetails = async (params: { group?: string, groupId?: number }) => {
+  const res = await makeGetRequest(path, params)
+  return res.body.Group
+}
+
+export default getGroupDetails
---
+++ src/requests/getGroupDetails.spec.js
@@ -0,0 +1,50 @@
+/* @flow */
+
+import test from 'ava'
+import nock from 'nock'
+
+import getGroupDetails from './getGroupDetails'
+
+test(`get the details for a group based on the group name`, async (t) => {
+  const GROUP_ID = '32744'
+  const GROUP = 'default'
+
+  nock('https://api.geni.us')
+    .get('/v1/group/get-details')
+    .query({
+      group: GROUP
+    })
+    .reply(200, {
+      Group: {
+        Name: GROUP,
+        Id: GROUP_ID
+      }
+    })
+
+  const groupDetails = await getGroupDetails({group: GROUP})
+
+  t.is(groupDetails.Name, GROUP)
+  t.is(groupDetails.Id, GROUP_ID)
+})
+
+test(`get the details for a group based on the group id`, async (t) => {
+  const GROUP_ID = '32744'
+  const GROUP = 'default'
+
+  nock('https://api.geni.us')
+    .get('/v1/group/get-details')
+    .query({
+      groupId: GROUP_ID
+    })
+    .reply(200, {
+      Group: {
+        Name: GROUP,
+        Id: GROUP_ID
+      }
+    })
+
+  const groupDetails = await getGroupDetails({groupId: GROUP_ID})
+
+  t.is(groupDetails.Name, GROUP)
+  t.is(groupDetails.Id, GROUP_ID)
+})
