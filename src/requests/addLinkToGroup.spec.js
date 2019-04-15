76a88682e95d45a011f876bb2bc7f068404ceca3
Lucy-Jane Walsh <ljewalsh@gmail.com>
2019-04-15 12:56:07 +1200

    Updates addLinkToGroup to use v3 endpoint and specifications

--- src/requests/addLinkToGroup.spec.js
+++ src/requests/addLinkToGroup.spec.js
@@ -11,31 +11,14 @@ test(`create a new tracked link`, async (t) => {
   const GROUP_ID = 12345
 
   nock('https://api.geni.us')
-    .post('/v1/links/add')
+    .post('/v3/shorturls')
     .reply(200, {
-      LinkResponses: [{
-        NewLink: {
-          ShortUrlCode: SHORTCODE
+        shortUrl: {
+          code: SHORTCODE
         }
-      }]
     })
 
   const shortcode = await addLinkToGroup(LINK, GROUP_ID)
 
   t.is(shortcode, SHORTCODE)
-})
-
-test('If genius replies with 200 but LinkResponses contains an error message, throw an error that we can easily see', async (t) => {
-  const LINK = 'http://app.mish.guru'
-  const GROUP_ID = 99870
-
-  nock('https://api.geni.us')
-    .post('/v1/links/add')
-    .reply(200, {
-      LinkResponses: [{
-        ErrorMessage: 'genius message'
-      }]
-    })
-
-  await t.throwsAsync(addLinkToGroup(LINK, GROUP_ID), AddLinkToGroupFailed)
 })
