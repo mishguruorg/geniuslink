aedb8889918824bbaab3f67b2ba0a0fd43076206
Lucy-Jane Walsh <ljewalsh@gmail.com>
2019-04-15 12:56:40 +1200

    Removes test script

--- test.js
+++
@@ -1,8 +0,0 @@
-const initGeniusLink = require('./dist/initGeniusLink').default
-const addLinkToGroup = require('./dist/requests/addLinkToGroup').default
-initGeniusLink({
-  key: process.env.GENIUSLINK_API_KEY,
-  secret: process.env.GENIUSLINK_API_SECRET
-})
-addLinkToGroup('http://www.stuff.co.nz', '81222')
-    .then(console.log)
